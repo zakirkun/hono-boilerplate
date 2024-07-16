874e48d11d868e621dcfb3144929198f4355ec69
Muhammad Zakir Ramadhan <61570975+zakirkun@users.noreply.github.com>
2024-07-16 18:19:50 +0700

    Add docker

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
 import { cors } from "hono/cors";
 import { UsersController } from "./src/controllers/users";
+import { env } from "bun";
 
 const app = new Hono();
 const userController = new UsersController;
@@ -24,6 +25,6 @@ app.post("/users", userController.createUsers);
 app.notFound((c) => c.json({ message: "Not Found" }, 404));
 
 export default {
-    port: 3000,
+    port: Bun.env.PORT || 3000,
     fetch: app.fetch,
 }
